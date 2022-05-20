2e0104d32dcdb700d1de2694b6ccd8eba8e9528d
Ali Emami <aliemami.net@gmail.com>
2022-05-20 23:47:32 +0430

    004: Fix init command bug

--- rating/management/commands/init.py
+++ rating/management/commands/init.py
@@ -3,35 +3,24 @@ from random import randrange
 
 from django.core.management import BaseCommand
 
-from ...models import *
+from ...models import Invoice
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        init_configurations()
-        init_cdr()
-
-
-def init_configurations():
-    configurations = Configurations.instance()
-    configurations.energy_fee = 0.3
-    configurations.time_fee = 2
-    configurations.transaction_fee = 1
-    configurations.time_format = "%Y-%m-%dT%H:%M:%SZ"
-    configurations.save()
-
-
-def init_cdr():
-    cdrs = []
-    for i in range(15):
-        meter_start = randrange(1200000, 1300000)
-        meter_start_time = datetime.now()
-        cdrs.append(CDR(
-            meterStart=meter_start,
-            meterStartTime=meter_start_time,
-            meterStop=meter_start + randrange(1000, 100000),
-            meterStopTime=meter_start_time + timedelta(hours=randrange(0, 2), minutes=randrange(0, 59),
-                                                       seconds=randrange(0, 59), ),
-        ))
-    CDR.objects.all().delete()
-    CDR.objects.bulk_create(cdrs)
+        invoices = []
+        for i in range(15):
+            meter_start = randrange(1200000, 1300000)
+            meter_start_time = datetime.now()
+            invoices.append(Invoice(
+                meterStart=meter_start,
+                timestampStart=meter_start_time,
+                meterStop=meter_start + randrange(1000, 100000),
+                timestampStop=meter_start_time + timedelta(hours=randrange(0, 2), minutes=randrange(0, 59),
+                                                           seconds=randrange(0, 59), ),
+                energy=0.3,
+                time=2,
+                transaction=1,
+            ))
+        Invoice.objects.all().delete()
+        Invoice.objects.bulk_create(invoices)
