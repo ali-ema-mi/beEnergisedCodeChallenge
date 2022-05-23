f69ac07c51821a6e601d3f3a989b642f62500280
Ali Emami <aliemami.net@gmail.com>
2022-05-23 10:08:38 +0430

    005: Add test for invoice endpoint

--- rating/tests.py
+++ rating/tests.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 
 from django.conf import settings
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from rest_framework import status
 
 from .models import Invoice
 
 
 class RatingTestCase(TestCase):
     def setUp(self):
-        pass
+        self.client = Client()
 
-    def test_rating_formula(self):
+    def test_pricing_formula(self):
+        '''
+        Tests pricing formula
+        :return: Test result
+        '''
         cdr = Invoice.objects.create(**{
             'meterStart': 1204307,
             'timestampStart': datetime.strptime("2021-04-05T10:04:00Z", settings.TIME_FORMAT),
@@ -22,3 +28,19 @@ class RatingTestCase(TestCase):
 
         })
         self.assertEqual(cdr.price(), 7.04)
+
+    #
+    def test_pricing_endpoint(self):
+        '''
+        Testing invoice endpoint
+        :return: Test result
+        '''
+        data = {
+            "rate": {"energy": 0.3, "time": 2, "transaction": 1},
+            "cdr": {"meterStart": 1204307, "timestampStart": "2021-04-05T10:04:00Z", "meterStop": 1215230,
+                    "timestampStop": "2021-04-05T11:27:00Z"},
+        }
+        response = self.client.post(reverse("cdr-list"), data=data, content_type="application/json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response = response.content.decode()
+        self.assertEqual(response, '{"overall":7.04,"components":{"energy":3.277,"time":2.767,"transaction":1.0}}')
